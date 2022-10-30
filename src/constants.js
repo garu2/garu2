c27715a43ab6d2c8eea22396efd29f4bc131acc8
garu2 <elgafo.nakama@gmail.com>
2022-10-30 18:49:43 -0400

    FIX: test6

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,8 @@
 export const NUMBER_OF = {
-    ARTICLES: 5,
-    PHOTOS: 4,
     VIDEOS: 3
   }
   
   export const PLACEHOLDERS = {
-    LATEST_ARTICLES: '%{{latest_articles}}%',
-    LATEST_YOUTUBE: '%{{latest_youtube}}%',
-    LATEST_YOUTUBE_SECONDARY: '%{{latest_youtube_secondary}}%',
-    LATEST_INSTAGRAM: '%{{latest_instagram}}%'
+    LATEST_YOUTUBE: '%{{latest_youtube}}%'
   }
   
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const YOUTUBE_BLACKCODE_CHANNEL_ID = 'UC1RSlIlxEmpuN6PUplzXpNw'
 const BLACKCODE_REACT = 'PL9c-AU5X8n1T4y1Y3VG-maQYyASZEg-4C';
 
 const { YOUTUBE_API_KEY } = process.env;
-
+console.log("env: ", YOUTUBE_API_KEY);
 const getLatestYoutubeVideos = () =>
   fetch(
     `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${BLACKCODE_REACT}&maxResults=3&key=${YOUTUBE_API_KEY}`
